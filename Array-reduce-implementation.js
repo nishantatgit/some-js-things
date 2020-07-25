c3833f1271c019de89c5df7aa45dfec659c3575f
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-07-25 21:24:34 +0530

    added logic to handle 2nd test case

--- Array-reduce-implementation.js
+++ Array-reduce-implementation.js
@@ -1,8 +1,15 @@
 (function (){
   Array.prototype.myReduce = function (fn,initialValue){
     var len = this.length;
-    var acc = initialValue || this[0];
-    var startIndex = initialValue ? 0 : 1;
+    var acc;
+    var startIndex = 0;
+   
+    while(startIndex < len && !(startIndex in this)){
+      startIndex += 1;
+    }
+
+    acc = arguments.length > 1 ? initialValue : this[startIndex++];
+    
     for(var i = startIndex; i< len; i++){
       acc = fn(acc,this[i],i,this)
     }
@@ -13,6 +20,13 @@
     var arr = [1,2,3,4];
     console.log(arr.myReduce((acc,v) => acc + v)); // 10
     console.log(arr.myReduce(function(acc,v){ return acc + v},5)) // 15
+
+    var arr2 = [];
+    arr2[5] = 2;
+    arr2[6] = 3;
+    arr2[7] = 4;
+    console.log(arr2.myReduce((acc,v) => acc + v)); // 9
+    console.log(arr2.myReduce(function(acc,v){ return acc + v},31)); // 40
   }
 
   testMyReducer();

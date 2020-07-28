e2c310c98162c03c7b789b37b30c17ff49e5487f
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-07-28 21:04:37 +0530

    selection sort

---
+++ sorting algorithms/selectionSort.js
@@ -0,0 +1,33 @@
+function selectionSort(array, sortFunction){
+  function defaultSortFunction(a,b){
+    return a-b;
+  }
+  
+  if(Object.prototype.toString.call(array) !== "[object Array]"){
+    throw new TypeError(array + ' is of invalid type');
+  }
+
+  if(!array){
+    throw new TypeError( array + ' is of invalid type');
+  }
+
+  var len = array.length;
+
+  if(len === 0 || len === 1) {
+    return array;
+  }
+
+  sortFunction = sortFunction || defaultSortFunction;
+
+  for(var i = 0; i < len - 1; i++){
+    for(var j = i+1 ; j < len ; j++){
+      if(sortFunction(array[i],array[j]) > 0){
+        var tmp = array[j];
+        array[j] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }  
+
+  return array;
+}

4099e51e3bda93ec84b7b5435f6373a1b4d4f9ed
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-07-29 13:18:25 +0530

    added insertion sort

---
+++ sorting algorithms/insertionSort.js
@@ -0,0 +1,31 @@
+function insertionSort(array, sortFunction){
+  function defaultSortFunction(a,b){
+    return a-b;
+  }
+  
+  if(Object.prototype.toString.call(array) !== "[object Array]"){
+    throw new TypeError(array + ' is of invalid type');
+  }
+
+  var len = array.length;
+
+  if(len === 0 || len === 1) {
+    return array;
+  }
+
+  sortFunction = sortFunction || defaultSortFunction;
+
+  for(var i = 1; i < len; i++){
+    for(var j = 0; j < i; j++){
+      if(sortFunction(array[j],array[i]) > 0){
+        var tmp = array[i];
+        for(k=i; k>=j; k--){
+          array[k] = arr[k-1];
+        }
+        array[j] = tmp;
+      }
+    }
+  }
+
+  return array;
+}

95f90b33c5b7310ad6418ab5be83703b72819b44
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-02 18:52:47 +0530

    heapsort related changes

--- sorting algorithms/heapSort.js
+++ sorting algorithms/heapSort.js
@@ -32,46 +32,21 @@ function heapSort(array, sortFunction){
       array[max] = tmp;
       
     }
-    console.log('array' ,array);
   }
 
   var mid = Math.floor(len/2);
 
   //build max heap;
   for(var i = mid; i >=0 ; i--){
-    console.log('i',i);
     maxHeapify(len-1,i);
   }
-
-  console.log('array after max heap', array);
 
   for(var i = len - 1; i > 0; i--){
     maxHeapify(i,0);
     var tmp = array[0];
     array[0] = array[i];
     array[i] = tmp;
-    console.log('maxHeapify ', array);
   }
 
   return array;
 }
-
-// 0, 1, 2, 3, 4 , 5, 6, 7, 8 
-
-// 0 2n + 1 = 2 * 0 + 1 = 1
-//   2n + 2 = 2 * 0 + 2 = 2
-
-// 1 2n + 1 = 2 * 1 + 1 = 3
-//   2n + 2 = 2 * 1 + 2 = 4
-
-// 2 2n + 1 = 2 * 2 + 1 = 5
-//   2n + 2 = 2 * 2 + 2 = 6
-
-// 3 2n + 1 = 2 * 3 + 1 = 7
-//   2n + 2 = 2 * 3 + 2 = 8
-
-// 4 2n + 1 = 2 * 4 + 1 = 9
-//   2n + 2 = 2 * 4 + 2 = 10
-
-// 5 2n + 1 = 2 * 5 + 1 = 11
-//   2n + 2 = 2 * 5 + 2 = 12

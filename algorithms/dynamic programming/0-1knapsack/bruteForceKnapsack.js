0f01af23d61b429eb80137283f72350fd72cccc9
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-05 23:20:40 +0530

    added input validation

--- algorithms/dynamic programming/0-1knapsack/bruteForceKnapsack.js
+++ algorithms/dynamic programming/0-1knapsack/bruteForceKnapsack.js
@@ -1,11 +1,16 @@
 function bruteForceKnapSack(val,weight,capacity,bag={}){
+  if(Object.prototype.toString.call(val) !== "[object Array]"){
+    throw new TypeError(array + ' is of invalid type, number array expected');
+  }
+  if(Object.prototype.toString.call(weight) !== "[object Array]"){
+    throw new TypeError(array + ' is of invalid type, number array expected');
+  }
   var len = val.length;
   var maxTotal = -Infinity;
-
+  var totalWeight = 0;
   for(var i = 0 ; i < len; i++){
     if(!bag[i]){
       bag[i] = true;
-      var totalWeight = 0
       if((capacity - val[i]) >= 0){
         totalWeight = weight[i] + bruteForceKnapSack(val,weight,capacity - val[i],bag);
       }

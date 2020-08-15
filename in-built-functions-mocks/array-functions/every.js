debe4f20d4c4d9eaa4409b305da5389cf6ea9f0b
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-15 13:17:27 +0530

    added array every function

---
+++ in-built-functions-mocks/array-functions/every.js
@@ -0,0 +1,30 @@
+(function(){
+
+  Array.prototype.myEvery = function myEvery(fn){
+
+    if(typeof fn !== 'function'){
+      throw new TypeError(fn + ' is not a valid function');
+    }
+
+    var len = this.length;
+    var result = true;
+
+    for(var i = 0; i < len; i++){
+      result = fn(this[i],i,this);
+      if(!result){
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  function testMyEvery(){
+    var arr = [-1,2,3,4];
+    console.log(arr.myEvery(v => v < 0)); // false
+    console.log(arr.mySome(v => v >= -1)); // true
+  }
+
+  testMyEvery();
+
+})();

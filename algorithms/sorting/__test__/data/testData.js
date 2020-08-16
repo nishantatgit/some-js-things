9502284ad8672492e088369b59c2b6933ac49886
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-16 21:34:00 +0530

    added test data

--- algorithms/sorting/__test__/data/testData.js
+++ algorithms/sorting/__test__/data/testData.js
@@ -14,7 +14,10 @@ var arraysToTest = {
           35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 
           20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 
           4, 3, 2, 1
-        ] // reverse sorted number array
+        ], // reverse sorted number array
+  '4' : [-1],
+  '5' : [2,1],
+  '6' : [1,-2,3]
 }
 
 export { arraysToTest as testData };

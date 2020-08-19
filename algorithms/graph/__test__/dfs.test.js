fad75fa256e1bdbc3d6399b087e0e58a90133a78
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-20 00:11:31 +0530

    added more test to dfs

--- algorithms/graph/__test__/dfs.test.js
+++ algorithms/graph/__test__/dfs.test.js
@@ -8,8 +8,8 @@ describe('depth first search ', function(){
     grph = graph();
   });
 
+  /* graph1 */
   it(' should return path based on depth first search ', function(){
-    
     grph.addVertex('a');
     grph.addVertex('b');
     grph.addVertex('c');
@@ -34,6 +34,7 @@ describe('depth first search ', function(){
     expect(dfs(grph, 'b')).toEqual(['b','c', 'a']);
   });
 
+  /* graph2 */
   it(' should return path based on depth first search when a vertex has path to itself', function(){
     grph.addVertex('a');
     grph.addVertex('b');
@@ -75,5 +76,62 @@ describe('depth first search ', function(){
     expect(dfs(grph, 'b')).toEqual(['b','c','d','a']);
     expect(dfs(grph, 'c')).toEqual(['c','a','b', 'd']);
     expect(dfs(grph, 'd')).toEqual(['d','a','b', 'c']);
+  });
+
+  /* graph3 */
+  it(' should return path based on depth first search when a there is cycle in graph', function(){
+    
+    grph.addVertex('a');
+    grph.addVertex('b');
+    grph.addVertex('c');
+    grph.addVertex('d');
+    grph.addVertex('e');
+
+    grph.addEdge({
+      startVertex: 'a',
+      endVertex: 'b',
+      identifier: 'a_b'
+    });
+    grph.addEdge({
+      startVertex: 'a',
+      endVertex: 'd',
+      identifier: 'a_d'
+    });
+    grph.addEdge({
+      startVertex: 'b',
+      endVertex: 'a',
+      identifier: 'b_a'
+    });
+    grph.addEdge({
+      startVertex: 'c',
+      endVertex: 'b',
+      identifier: 'c_b'
+    });
+    grph.addEdge({
+      startVertex: 'c',
+      endVertex: 'd',
+      identifier: 'c_d'
+    });
+    grph.addEdge({
+      startVertex: 'd',
+      endVertex: 'e',
+      identifier: 'd_e'
+    });
+    grph.addEdge({
+      startVertex: 'e',
+      endVertex: 'b',
+      identifier: 'e_b'
+    });
+    grph.addEdge({
+      startVertex: 'e',
+      endVertex: 'c',
+      identifier: 'e_c'
+    });
+
+    expect(dfs(grph, 'a')).toEqual(['a','b','d','e','c']);
+    expect(dfs(grph, 'b')).toEqual(['b','a','d','e','c']);
+    expect(dfs(grph, 'c')).toEqual(['c','b','a','d','e']);
+    expect(dfs(grph, 'd')).toEqual(['d','e','b','a','c']);
+    expect(dfs(grph, 'e')).toEqual(['e','b','a','d','c']);
   });
 })

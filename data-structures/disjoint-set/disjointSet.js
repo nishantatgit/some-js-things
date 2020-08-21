3a3ccdbba12b47f7b5478a573948a3babf72af0e
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-21 19:35:29 +0530

    added disjoint set skeleton

---
+++ data-structures/disjoint-set/disjointSet.js
@@ -0,0 +1,23 @@
+function disjointSet(list){
+  var disjointSets = {};
+
+  function find(){
+
+  }
+
+  function union(){
+
+  }
+
+  function makeSet(){
+
+  }
+
+  return {
+    find : find,
+    union: union,
+    makeSet: makeSet 
+  }
+}
+
+export { disjointSet };

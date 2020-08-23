ff9a454c0e8a2272b1d0fb7c3a1decccc796f315
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-08-23 23:32:03 +0530

    added binary tree skeleton

---
+++ data-structures/tree/binaryTree.js
@@ -0,0 +1,65 @@
+function binaryTree(){
+  function createNode(value,left,right){
+    return {
+      value: value,
+      left: left,
+      right: right
+    }
+  }
+
+  function insertAt(node){
+
+  }
+
+  function insertAtLeft(node){
+
+  }
+
+  function insertAtRight(node){
+
+  }
+
+  function size(){
+    return size;
+  }
+
+  function contains(){
+    //Determines if a particular element is in the tree
+  }
+
+  function find(){
+    // Returns a reference to the specified target, if found
+  }
+
+  function inOrderTraversal(){
+
+  }
+
+  function preOrderTraversal(){
+
+  }
+
+  function postOrderTraversal(){
+
+  }
+
+  function levelOrderTraversal(){
+
+  }
+
+  function levelOrderAlternateTraversal(){
+
+  }
+
+  function isEmpty(){
+    return root === null;
+  }
+
+  function removeNode(node){
+
+  }
+
+  var root = null;
+  var size = 0;
+
+}
